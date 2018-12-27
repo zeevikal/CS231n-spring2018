aa552344beda86cbb021162cac311f3e27ebd3b7
Zeev Kalyuzhner <zeevikal@gmail.com>
2018-12-27 11:12:52 +0200

    added q3 - softmax

--- assignment1/cs231n/classifiers/softmax.py
+++ assignment1/cs231n/classifiers/softmax.py
@@ -29,7 +29,27 @@ def softmax_loss_naive(W, X, y, reg):
   # here, it is easy to run into numeric instability. Don't forget the        #
   # regularization!                                                           #
   #############################################################################
-  pass
+  scores = X.dot(W)
+  num_train = X.shape[0]
+  num_classes = W.shape[1]
+
+  # Softmax Loss
+  for i in range(num_train):
+    f = scores[i] - np.max(scores[i])
+    softmax = np.exp(f)/np.sum(np.exp(f))
+    loss += -np.log(softmax[y[i]])
+    # Weight Gradients
+    for j in range(num_classes):
+      dW[:,j] += X[i] * softmax[j]
+    dW[:,y[i]] -= X[i]
+
+  # Average
+  loss /= num_train
+  dW /= num_train
+
+  # Regularization
+  loss += reg * np.sum(W * W)
+  dW += reg * 2 * W 
   #############################################################################
   #                          END OF YOUR CODE                                 #
   #############################################################################
@@ -46,14 +66,32 @@ def softmax_loss_vectorized(W, X, y, reg):
   # Initialize the loss and gradient to zero.
   loss = 0.0
   dW = np.zeros_like(W)
-
+    
   #############################################################################
   # TODO: Compute the softmax loss and its gradient using no explicit loops.  #
   # Store the loss in loss and the gradient in dW. If you are not careful     #
   # here, it is easy to run into numeric instability. Don't forget the        #
   # regularization!                                                           #
   #############################################################################
-  pass
+  num_train = X.shape[0]
+  
+  # scores have number of examples rows and classes columns
+  scores = X.dot(W)
+  scores -= np.max(scores)  # trick that control number instability
+  # this gives the correct class score for each example
+  correct_class_score = scores[np.arange(num_train), y].reshape(-1, 1)
+
+  exp_sum = np.sum(np.exp(scores), axis=1).reshape(-1, 1)
+  loss = np.sum(- correct_class_score + np.log(exp_sum))
+  
+  softmax = np.exp(scores) / exp_sum
+  grad = softmax
+  grad[np.arange(num_train), y] -= 1
+  dW = X.T.dot(grad)
+  loss /= num_train
+  dW /= num_train
+  loss += reg * np.sum(W * W)
+  dW += reg * 2 * W
   #############################################################################
   #                          END OF YOUR CODE                                 #
   #############################################################################
